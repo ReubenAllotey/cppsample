d1a2fac5e7879b05539def7b91c5f7ef89ce8c9c
ReubenAllotey <reubenallotey434@gmail.com>
2024-06-17 23:49:59 +0000

    Added a new directory to repo

---
+++ CLionProjects/course.cpp/main.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+using namespace std;
+int main() {
+//    cout << "Hello, World!" <<endl;
+
+  //data types
+  //storing numbers
+  //short num1=2;//2byte /-32KTO 32K
+ // int numb2=5;//4byte /-32b TO 32b
+//long num3=234;//8byte
+//unsigned short num4=363; //unsigned is used to remove negative numbers.
+
+//storing  decimals .
+//float num5=45.88;
+//double num3=12.78;
+
+//storing characters
+//char numb7 ='&';
+
+//boolean data types
+// bool isHappy = true; //"true or false "
+
+//int number;
+//cout <<"enter a number! ";
+//cin>>number;
+//if (number >=45){
+//    cout <<"good number";
+//    } else if (number==30){
+//    cout<<"Not bad!";
+//}else {
+//    cout <<"better luck next time !";
+//}
+//switch (number){
+//    case 5:
+//        cout<<"number is good !";
+//        break;
+//        case2:
+//            cout<<"number is bad!";
+//            break;
+//    default:
+//        cout <<"nextime!";
+//}
+// int num1;
+// int num2;
+// cout <<"enter values of num1 && num2";
+// cin>>num1>>num2;
+// if (num1>num2){
+//     cout<<"num1 is greater!";
+// }
+// else if(num1<num2){
+//     cout<<"num2 is greater!";
+// }
+// else {
+//     cout<<"num1 is equal to num2!";
+// }
+//if (num1-num2<5){
+//    cout<<"the difference  between the numbers is small!";
+//}
+//if (num1/2){
+//    cout<<"num1 is an even number";
+//
+//}
+
+//generating random numbers  /calculator development
+// float num1,num2,res;
+// cout <<"enter num1 , num2 ";
+//  cin>>num1>>num2;
+//char mathsOperation ;
+//cout<<"enter mathsOperation";
+//cin>>mathsOperation;
+//
+// if (mathsOperation=='+'){
+//     res=(num1+num2);
+// }else if (mathsOperation=='-'){
+//     res=(num1-num2);
+// }else if (mathsOperation=='/') {
+//     res = (num1 / num2);
+// }else if (mathsOperation=='*') {
+//     res = (num1 * num2);
+// }
+//     cout << res;
+
+// to generate random numbers
+
+// looping /for loop/while loop/do while loop
+
+//for (int i=0;i<10;i++){
+//    cout<<i<<endl;
+//}
+
+//int i;
+//while (i<10) {
+//    cout << i << endl;
+//    i++;
+//}
+//int i=100;
+//do{
+//  cout<<i<<endl;
+//  i-=10;
+//}while(i<10);
+
+// loop operations /break operator/ continue operator
+
+
+for (int i=0;i<15;i++){
+    if (i==10)
+    break;
+
+}if (i)
+
+
+
+
+
+
+     return 0;
+
+ }
---
+++ CLionProjects/learningcpp/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+int main(){
+ // int a=100;
+ // int b =200;
+  //int c=a;
+ // a=b;
+  //b=c;
+//int file_size;//snake case
+//int File_size;//pascal case
+//int fileSize;// camel case
+
+//int x=23;
+//int y=13;
+//int z=x+y;
+//double x=10;
+//double y=5;
+//double z=(x+10)/(3*y);
+// double sales =9500;
+// cout<<"sales:$"<<sales<<endl;
+//
+// double stateTaxRate=0.04;
+// double stateTax=(stateTaxRate *sales);
+// cout<<"stateTax:"<<stateTax<<endl;
+//
+// double countyTaxRate =0.02;
+// double countyTax=(countyTaxRate *sales);
+// cout<<"countyTax:$"<<countyTax<<endl;
+//
+//double totalTax=(countyTax +stateTax);
+//cout<< "totalTax:$"<< totalTax<<endl<
+
+//cout<<"fahrenheit:";
+//int fahrenheit;
+//cin >>fahrenheit;
+//double celsius =(fahrenheit-32)/1.8;
+//cout<<celsius;
+
+//cout<<"radius:";
+//int radius ;
+//cin>>radius;
+//double area =(3.142*radius*radius);
+//cout<<area;
+//changing  from binary number and hexadecimal numbers  to real numbers
+//fom binary to actual numbers
+//int number =0b11111111;
+//cout<<number;
+//changing from hexadecimal to actual numbers
+//int number=0xae;
+//cout<< number;
+
+
+
+
+
+
+
+   return 0;
+
+}
